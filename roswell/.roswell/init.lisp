531735e0598117b03a84a1f36ae928122df27a91
sakurawald <sakurawald@gmail.com>
2024-11-20 08:49:39 +0800

    sync

---
+++ roswell/.roswell/init.lisp
@@ -0,0 +1,1 @@
+(sb-ext:set-sbcl-source-location "~/.roswell/src/sbcl-2.4.10/")
--- sync.sh
+++ sync.sh
@@ -6,6 +6,9 @@ cp ~/.config/nvim/init.vim ./nvim/
 
 cp ~/.i3/config ./i3/.i3
 
+cp ~/.roswell/init.lisp ./roswell/.roswell/
+
+
 git add .
 git commit -m "sync"
 git push
