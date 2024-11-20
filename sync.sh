c44ece2ee25cda9ccb48e44baceea0f7188a598d
sakurawald <sakurawald@gmail.com>
2024-11-20 09:21:43 +0800

    sync

--- sync.sh
+++ sync.sh
@@ -1,14 +1,30 @@
+# lem editor
 cp ~/.lem/init.lisp ./lem/ 
 
+# jetbrains: ideavim plugin
 cp ~/.ideavimrc ./ideavim/
 
+# nvim editor
 cp ~/.config/nvim/init.vim ./nvim/
 
+# i3 window manager
 cp ~/.i3/config ./i3/.i3
 
+# roswell launcher
 cp ~/.roswell/init.lisp ./roswell/.roswell/
 
+# tmux multiplexer
+cp ~/.tmux.conf ./tmux
 
+# spicy
+cp ~/.config/spicy/settings ./spicy/
+
+# ranger
+cp ~/.config/ranger/rc.conf ./ranger/
+cp ~/.config/ranger/rifle.conf ./ranger/
+
+# git
 git add .
 git commit -m "sync"
 git push
+echo "=== Remember to check the result file ==="
